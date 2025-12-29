27cf73409d316646a3e87f8cf6c47c4f635000a2
Authcornelius <corneliusedos@gmail.com>
2025-12-29 06:37:44 +0100

    404 page updated

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,18 +1,20 @@
-import Link from 'next/link';
-import { FaHome } from 'react-icons/fa'; 
+import Link from "next/link";
+import { FaHome } from "react-icons/fa";
 
 const Custom404 = () => {
   return (
     <div className="min-h-screen bg-gradient-to-r from-purple-500 to-pink-500 flex flex-col justify-center items-center text-white">
       {/* Main Heading */}
       <h1 className="text-6xl font-bold mb-4">Oops! 😱</h1>
+
       <p className="text-2xl mb-8">
-        We can't seem to find the page you're looking for.
+        We can&apos;t seem to find the page you&apos;re looking for.
       </p>
 
       {/* Funny Message */}
       <p className="text-lg mb-8 text-center max-w-md">
-        Maybe it was abducted by aliens 👽, or it just took a wrong turn into the Internet wilderness! Either way, it's not here.
+        Maybe it was abducted by aliens 👽, or it just took a wrong turn into the
+        Internet wilderness! Either way, it&apos;s not here.
       </p>
 
       {/* Button to navigate back */}
@@ -25,6 +27,6 @@ const Custom404 = () => {
       </Link>
     </div>
   );
-}
+};
 
 export default Custom404;
